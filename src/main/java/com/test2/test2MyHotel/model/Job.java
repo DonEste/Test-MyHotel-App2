8d273f210eca91aed7b03e871fd404672f22e17f
Matias Carreño <30509507+DonEste@users.noreply.github.com>
2021-03-21 17:34:46 -0300

    Entities
    -Employee
    -JobHistory
    -Job

---
+++ src/main/java/com/test2/test2MyHotel/model/Job.java
@@ -0,0 +1,71 @@
+package com.test2.test2MyHotel.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "jobs")
+public class Job {
+    private int jobId;
+    private String jobTitle;
+    private double minSalary;
+    private double maxSalary;
+    @OneToMany(mappedBy = "jobs", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Employee> employees = new ArrayList<>();
+
+    public Job() {
+    }
+
+    public Job(int jobId, String jobTitle, double minSalary, double maxSalary) {
+        this.jobId = jobId;
+        this.jobTitle = jobTitle;
+        this.minSalary = minSalary;
+        this.maxSalary = maxSalary;
+    }
+
+    public int getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(int jobId) {
+        this.jobId = jobId;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public double getMinSalary() {
+        return minSalary;
+    }
+
+    public void setMinSalary(double minSalary) {
+        this.minSalary = minSalary;
+    }
+
+    public double getMaxSalary() {
+        return maxSalary;
+    }
+
+    public void setMaxSalary(double maxSalary) {
+        this.maxSalary = maxSalary;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+}
