d76ed59f9c3e5beaa79fe3f4b5463683fb1212e1
Matias Carreño <30509507+DonEste@users.noreply.github.com>
2021-03-21 15:45:54 -0300

    init commit

---
+++ src/main/java/com/test2/test2MyHotel/Test2MyHotelApplication.java
@@ -0,0 +1,13 @@
+package com.test2.test2MyHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test2MyHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test2MyHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test2/test2MyHotel/Test2MyHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test2.test2MyHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test2MyHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
