0e2292e70f00c08b0616deadbe4e2180e8573450
Matias Carreño <30509507+DonEste@users.noreply.github.com>
2021-03-22 13:21:29 -0300

    first functional Endpoint
    -Employee by salary

--- src/main/java/com/test2/test2MyHotel/service/EmployeeService.java
+++ src/main/java/com/test2/test2MyHotel/service/EmployeeService.java
@@ -2,7 +2,7 @@ package com.test2.test2MyHotel.service;
 
 import com.test2.test2MyHotel.model.Employee;
 import com.test2.test2MyHotel.repository.EmployeeRepository;
-
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +11,11 @@ public class EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    public Employee getEmployeeById(int id){
-        return employeeRepository.findById(id).orElse(null);
+    public List<Employee> getEmployees(){
+    	return employeeRepository.findAll();
+    }
+    
+    public List<Employee> getemployeesBySalary(int lowRange, int highRange){
+    	return employeeRepository.findSalaryRange((double)lowRange, (double)highRange);
     }
 }
